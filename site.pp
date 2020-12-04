697791d413aee6f745c0c5c4e87435d59eae0b54
mykolasjay <53540390+mykolasjay@users.noreply.github.com>
2020-12-04 17:12:35 +0200

    Update site.pp

--- site.pp
+++ site.pp
@@ -1,7 +1,12 @@
 node default {
-file {'/root/READMETST':
-  ensure => file,
-  content => 'This is a readme',
-  owner => 'mykolas',
+  file {'/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner => 'root',
   }
+  file {'/root/README2':
+    ensure => file,
+    content => 'This is a readme2',
+    owner => 'mykolas',  
+}
 }
